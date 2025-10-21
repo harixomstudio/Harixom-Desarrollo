2d42da89e0b469d2d4e6ca466cd27dfbe80f77cc
ByTricuxs <150404506+ByTricuxs@users.noreply.github.com>
2025-10-21 00:51:36 -0600

    Intento2

---
+++ backend/database/migrations/2025_10_21_063736_create_subscriptions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+    Schema::create('subscriptions', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->string('stripe_subscription_id')->nullable();
+        $table->string('stripe_customer_id')->nullable();
+        $table->string('plan_type'); // 'monthly' o 'annual'
+        $table->enum('status', ['active', 'canceled', 'incomplete'])->default('incomplete');
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
