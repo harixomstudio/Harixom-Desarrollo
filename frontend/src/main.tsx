6560bac7f09c128c9a80dcc28a746f112141a2a2
ByTricuxs <150404506+ByTricuxs@users.noreply.github.com>
2025-10-25 22:36:01 -0600

    Intento 3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -19,7 +19,6 @@ const router = createRouter({
 
 const queryClient = new QueryClient();
 
-// Necesario para que TS conozca el tipo de router
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
@@ -30,7 +29,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}> 
     <ToastProvider>
-        {/* El router se encarga de renderizar las rutas */}
         <RouterProvider router={router} />
       </ToastProvider>
     </QueryClientProvider>
